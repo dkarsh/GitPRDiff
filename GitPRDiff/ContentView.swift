e7682c2d7fde0c4caeddf291a96de7c7642c89da
daniel karsh <daniel.karsh@gmail.com>
2021-03-05 11:32:28 -0500

    Testing PR API call

--- GitPRDiff/ContentView.swift
+++ GitPRDiff/ContentView.swift
@@ -8,6 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    func loadData() {
+        
+        UIDevice.current.setValue(UIInterfaceOrientation.landscapeLeft.rawValue, forKey: "orientation")
+        UIViewController.attemptRotationToDeviceOrientation()
+        
+        let url = URL(string: "https://api.github.com/repos/magicalpanda/MagicalRecord/pulls/1293")!
+        var request = URLRequest(url: url)
+        request.setValue("keep-alive", forHTTPHeaderField: "Connection")
+        request.setValue("application/vnd.github.VERSION.diff", forHTTPHeaderField: "Accept")
+        request.httpMethod = "GET"
+        
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            
+        }
+        
+    }
     var body: some View {
         Text("Hello, world!")
             .padding()
