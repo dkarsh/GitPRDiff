2739fba8b7a9f9692d6b786ed2f4de98187fae71
daniel karsh <daniel.karsh@gmail.com>
2021-03-05 11:18:35 -0500

    Initial Commit

---
+++ GitPRDiff/GitPRDiffApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPRDiffApp.swift
+//  GitPRDiff
+//
+//  Created by Daniel Karsh on 3/5/21.
+//
+
+import SwiftUI
+
+@main
+struct GitPRDiffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
