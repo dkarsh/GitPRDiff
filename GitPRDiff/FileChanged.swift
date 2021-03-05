931835827ab5f5a9c5f559eff92902419b4bd55f
daniel karsh <daniel.karsh@gmail.com>
2021-03-05 11:47:23 -0500

    Adding FileChanged model to parse the text file

---
+++ GitPRDiff/FileChanged.swift
@@ -0,0 +1,16 @@
+//
+//  FileChanged.swift
+//  GitPRDiff
+//
+//  Created by Daniel Karsh on 3/5/21.
+//
+
+import Foundation
+
+struct FileChanged {
+    var id:Int
+    var title: String
+    var subTitle: String
+    var sideA:[String]
+    var sideB:[String]
+}
